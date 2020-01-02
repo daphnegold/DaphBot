e084eec98a97f1991d59c1cc9407d491f8c07f59
Daphne <daphne@moz.com>
2020-01-02 01:04:34 -0800

    Check prefix

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const Discord = require('discord.js');
+var fp = require('lodash-fp');
+
 const client = new Discord.Client();
 
 require('dotenv').config()
 const AUTH_TOKEN = process.env.AUTH_TOKEN;
+
+const PREFIX = '!';
+const hasPrefix = fp.startsWith(PREFIX);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}! Ready Freddy!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong <3');
+  if (msg.content.toLowerCase().match(/senpai/)) {
+    msg.channel.send('<3');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase().match(/senpai/)) {
-    msg.reply('<3');
+  const content = msg.content;
+
+  if (!hasPrefix(content)) return;
+
+  let args = content.substring(PREFIX.length).split(' ');
+
+  if (fp.isEqual(fp.head(args), 'startraid')) {
+    msg.reply('what raid would you like to start?')
   }
 });
 
